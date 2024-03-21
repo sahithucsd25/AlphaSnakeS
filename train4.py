04960f56941da77c673c597dd8a8124ceca26a0d
MatthewK0617 <kimmatthew2022@gmail.com>
2024-03-21 14:38:22 -0700

    gpu integration

--- train4.py
+++ train4.py
@@ -36,7 +36,7 @@ class ReplayBuffer:
     def __len__(self):
         return len(self.buffer)
 
-def update_policy(net, target_net, optimizer, memory, batch_size, gamma):
+def update_policy(net, target_net, optimizer, memory, batch_size, gamma, device):
     if len(memory) < batch_size:
         return
     states, actions, rewards, next_states, dones = memory.sample(batch_size)
@@ -46,11 +46,11 @@ def update_policy(net, target_net, optimizer, memory, batch_size, gamma):
     next_states = np.array(next_states)
     dones = np.array(dones)
 
-    states = torch.FloatTensor(states)
-    next_states = torch.FloatTensor(next_states)
-    actions = torch.LongTensor(actions)
-    rewards = torch.FloatTensor(rewards)
-    dones = torch.FloatTensor(dones)
+    states = torch.FloatTensor(states).to(device)
+    next_states = torch.FloatTensor(next_states).to(device)
+    actions = torch.LongTensor(actions).to(device)
+    rewards = torch.FloatTensor(rewards).to(device)
+    dones = torch.FloatTensor(dones).to(device)
 
     current_q_values = net(states).gather(1, actions.unsqueeze(1)).squeeze(1)
     next_q_values = target_net(next_states).max(1)[0]
@@ -70,14 +70,15 @@ def set_seed(seed_value=42):
     torch.backends.cudnn.deterministic = True
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # grid_size = "06x06"
     width = 6
     env = gym.make('snake-v0', grid_size=[width, width], n_foods=1)
     env.seed(42)
     n_actions = env.action_space.n
 
-    policy_net = DQN(in_channels=6, n_actions=n_actions)
-    target_net = deepcopy(policy_net)
+    policy_net = DQN(in_channels=6, n_actions=n_actions).to(device)
+    target_net = deepcopy(policy_net).to(device)
     for param in target_net.parameters():
         param.requires_grad = False
 
@@ -88,7 +89,10 @@ def main():
     epsilon_start = 1.0
     epsilon_final = 0.01
     epsilon_decay = 500
-    episodes = 1000
+    episodes = 5000
+
+    fruit_100 = []
+    steps_100 = []
 
     steps_done = 0
     best_total_reward = -np.inf
@@ -101,6 +105,8 @@ def main():
         state = np.transpose(state, (2, 0, 1))
         prev_state = np.zeros_like(state)  # Initialize prev state as zeros
         
+        total_steps = 0
+        fruits_eaten = 0
         total_reward = 0
         done = False
         while not done:
@@ -112,17 +118,21 @@ def main():
             epsilon = epsilon_final + (epsilon_start - epsilon_final) * \
                       np.exp(-1. * steps_done / epsilon_decay)
             steps_done += 1
+            total_steps += 1
 
             if random.random() < epsilon:
                 action = env.action_space.sample()
             else:
                 with torch.no_grad():
-                    combined_state_tensor = torch.FloatTensor(combined_state).unsqueeze(0)
+                    combined_state_tensor = torch.FloatTensor(combined_state).unsqueeze(0).to(device)
                     action = policy_net(combined_state_tensor).max(1)[1].view(1, 1).item()
 
             next_state, reward, done, _ = env.step(action)
             next_state = np.transpose(next_state, (2, 0, 1))
             next_combined_state = np.concatenate((state, next_state), axis=0)  # Next combined state for memory
+
+            if (reward == 3):
+                fruits_eaten += 1
             
             memory.push(combined_state, action, reward, next_combined_state, done)
             
@@ -130,21 +140,30 @@ def main():
             total_reward += reward
             prev_state = state.copy()  # Update the previous state
 
-            update_policy(policy_net, target_net, optimizer, memory, batch_size, gamma)
+            update_policy(policy_net, target_net, optimizer, memory, batch_size, gamma, device)
         # plt.close()
         # env.close()
         
 
         if episode % 10 == 0:  # Update the target network
             target_net.load_state_dict(policy_net.state_dict())
+        
+        fruit_100.append(fruits_eaten)
+        steps_100.append(total_steps)
+
+        if episode % 100 == 0:
+            print(f"Episode: {episode}, 100 Fruit Average: {np.mean(fruit_100)}, 100 Step Average: {np.mean(steps_100)}")
+            fruit_100 = []
+            steps_100 = []
+            
 
         if total_reward > best_total_reward:
             best_total_reward = total_reward
             # Save the model
-            print(f"New best model with total reward: {total_reward}, at episode: {episode}")
+            # print(f"New best model with total reward: {total_reward}, at episode: {episode}")
             torch.save(policy_net.state_dict(), "best_policy_net.pth")
         
-            print(f'Episode {episode}, Total reward: {total_reward}, Epsilon: {epsilon:.2f}')
+            # print(f'Episode {episode}, Total reward: {total_reward}, Epsilon: {epsilon:.2f}')
 
     env.close()
 
